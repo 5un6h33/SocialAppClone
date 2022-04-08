ef99dd341758b92fb9da47412c40caebf764b0dc
hanseonghui <pinedelta@icloud.com>
2022-04-08 16:53:40 +0900

    Update Timeline

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 const { route } = require("./users");
 
 // create a post
